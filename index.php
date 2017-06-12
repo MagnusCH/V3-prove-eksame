68afc3eb20995f9bc244140db063c21fe9e7659e
Magnus Søndergaard Christensen <magn0811@edu.campusvejle.dk>
2017-06-12 12:39:49 +0200

    index layout done, css now

--- index.php
+++ index.php
@@ -4,8 +4,126 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
-</head>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/flexboxgrid.css">
+    <link rel="stylesheet" href="css/variabler.css">
+    <link rel="stylesheet" href="css/style.css"> </head>
 
-<body> </body>
+<body>
+    <!-- header -->
+    <header id="main-header">
+        <div class="header-container">
+            <div class="row end-xs end-sm end-md end-lg center-xs middle-xs middle-sm middle-md middle-lg ">
+                <div class="col-xs-4">
+                    <h1>Islændere i DK</h1></div>
+                <div class="col-xs-8"><img src="img/medlem-emblem.png" alt="Medlem emblem Logo"> </div>
+            </div>
+            <div class="row end-xs end-sm end-md end-lg center-xs middle-xs middle-sm middle-md middle-lg">
+                <div class="col-xs-4">
+                    <nav id="navbar">
+                        <ul>
+                            <li><a href="#" class="current">Forside</a></li>
+                            <li><a href="#">Nyheder</a></li>
+                            <li><a href="#">Sitemap</a></li>
+                            <li><a href="#">Print</a></li>
+                            <li><a href="#">Kontakt</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-xs-8">
+                    <form>
+                        <input type="text" name="search" placeholder="Search.."> </form>
+                </div>
+            </div>
+            <div class="row end-xs end-sm end-md end-lg center-xs middle-xs middle-sm middle-md middle-lg">
+                <div class="col-xs-12">
+                    <form action="">
+                        <input type="text" name="brugernavn" placeholder="Brugernavn...">
+                        <input type="password" name="password" placeholder="Password...">
+                        <button>Log in</button>
+                    </form>
+                </div>
+            </div>
+            <div class="row end-xs end-sm end-md end-lg center-xs middle-xs middle-sm middle-md middle-lg">
+                <div class="col-xs-12"> <a href="#" alt="Registrer dig her knap">Registrér dig her...</a> </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="main-container">
+            <div class="row center-xs center-sm center-md center-lg">
+                <div class="col-xs-3">
+                    <article><img src="img/articleImg1.jpg" alt="Article img 1">
+                        <h2>Article 1</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid soluta placeat at hic cupiditate vel modi, natus omnis. Omnis perspiciatis molestias modi quam deserunt tempora corrupti atque dolorem corporis dolore!</p>
+                    </article>
+                    <section id="dokumenter"> <i class="fa fa-file-text-o  fa-3x"></i>
+                        <h3>Dokumenter</h3> </section>
+                    <article><img src="img/articleImg2.jpg" alt="Article img 2">
+                        <h2>Article 2</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores inventore, molestias fugiat quo blanditiis non vitae maiores nobis, quaerat eaque commodi enim earum corporis est amet accusantium nostrum, ratione sequi.</p>
+                    </article>
+                </div>
+                <div class="col-xs-3">
+                    <section id="link"> <i class="fa fa-link fa-3x"></i>
+                        <h3>Links</h3> </section>
+                    <article> <img src="img/articleImg3.jpg" alt="Article img 3">
+                        <h2>Article 3</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias inventore, itaque voluptas perferendis distinctio laboriosam at! Ipsum enim, ut cum? Neque, voluptatibus ipsam eveniet commodi omnis voluptates nemo pariatur alias.</p>
+                    </article>
+                    <article> <img src="img/articleImg4.jpg" alt="Article img 4">
+                        <h2>Article 4</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur eius deserunt minus eos, earum, quo iusto expedita, nesciunt magni dolore aperiam! Quia assumenda amet ipsam vero expedita beatae, velit non!</p>
+                    </article>
+                </div>
+                <div class="col-xs-3">
+                    <article><img src="img/articleImg5.jpg" alt="Article img 5">
+                        <h2>Article 5</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, et reiciendis temporibus at laudantium voluptatibus, in repellat itaque saepe, quia dolore eius. Neque cupiditate necessitatibus, placeat ipsa laudantium eligendi eum.</p>
+                    </article>
+                    <section id="marked"> <i class="fa fa-shopping-cart fa-3x"></i>
+                        <h3>Marked</h3> </section>
+                    <article> <img src="img/articleImg6.jpg" alt="Article img 6">
+                        <h2>Article </h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas eius nisi quo, eligendi? Ullam repudiandae veniam officiis porro tempora eaque, natus quasi quae, accusantium, neque quam accusamus earum aperiam. Cumque!</p>
+                    </article>
+                </div>
+                <div class="col-xs-3">
+                    <section id="events"> <i class="fa fa-calendar fa-3x"></i>
+                        <h3>Events</h3> </section>
+                    <article> <img src="img/articleImg7.jpg" alt="Article 7 img">
+                        <h2>Article 7</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste illum itaque dignissimos facilis, dolor eius harum. Dicta quos neque at deserunt dolores, quo veniam nisi. Officiis veniam, commodi saepe maxime.</p>
+                    </article>
+                    <article> <img src="img/articleImg8.jpg" alt="Article 8 img">
+                        <h2>Article 8</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum delectus, repellat, suscipit porro, ducimus inventore id nobis, perspiciatis necessitatibus assumenda quas architecto. Sequi dignissimos ex, itaque quod ut voluptas, quae.</p>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer id="main-footer">
+        <div class="footer-container">
+            <div class="row  center-xs  ">
+                <div class="col-xs-4 ">
+                    <p>Islændere i DK</p>
+                    <p>Tågehornvej 32</p>
+                    <p>9300 Ølstykke</p>
+                    <p>islandere(at)live.com</p>
+                    <p><i class="fa fa-phone" aria-hidden="true"></i> 12345678</p>
+                </div>
+                <div class="col-xs-4">
+                    <p>Mandag-fredag:</p>
+                    <p>08:00-13:00</p>
+                    <p>Lørdag:</p>
+                    <p>09:00-11:00</p>
+                </div>
+                <div class="col-xs-4"> <img src="img/guy.jpg" alt="Thomas Karse img"> <span>Thomas Karse</span> <img src="img/guy2.jpg" alt="Bjarne Arm img"> <span>Bjarne Arm</span> <img src="img/guy3.jpg" alt="Lisbeth Grøn img"> <span>Lisbeth Grøn</span> </div>
+            </div>
+        </div>
+    </footer>
+</body>
 
 </html>
