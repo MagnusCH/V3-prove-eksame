31a67d000fd2b78db31b779e7181da04c3114e3c
Magnus Søndergaard Christensen <magn0811@edu.campusvejle.dk>
2017-06-12 14:08:25 +0200

    Index layout lg done

--- index.php
+++ index.php
@@ -4,10 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <!-- Normalize -->
     <link rel="stylesheet" href="css/normalize.css">
+    <!-- Font-awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Google fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Libre Baskerville' rel='stylesheet'>
+    <!-- Flexbox grid -->
     <link rel="stylesheet" href="css/flexboxgrid.css">
+    <!-- Variabler -->
     <link rel="stylesheet" href="css/variabler.css">
+    <!-- Style -->
     <link rel="stylesheet" href="css/style.css"> </head>
 
 <body>
@@ -15,11 +23,11 @@
     <header id="main-header">
         <div class="header-container">
             <div class="row end-xs end-sm end-md end-lg center-xs middle-xs middle-sm middle-md middle-lg ">
-                <div class="col-xs-4">
+                <div class="col-xs-6">
                     <h1>Islændere i DK</h1></div>
-                <div class="col-xs-8"><img src="img/medlem-emblem.png" alt="Medlem emblem Logo"> </div>
+                <div class="col-xs-6"><img src="img/medlem-emblem.png" alt="Medlem emblem Logo"> </div>
             </div>
-            <div class="row end-xs end-sm end-md end-lg center-xs middle-xs middle-sm middle-md middle-lg">
+            <div class="row end-xs end-sm end-md end-lg center-xs middle-xs middle-sm middle-md middle-lg margin-bot">
                 <div class="col-xs-4">
                     <nav id="navbar">
                         <ul>
@@ -33,20 +41,20 @@
                 </div>
                 <div class="col-xs-8">
                     <form>
-                        <input type="text" name="search" placeholder="Search.."> </form>
+                        <input id="seach" type="text" name="search" placeholder="Search.."> </form>
                 </div>
             </div>
             <div class="row end-xs end-sm end-md end-lg center-xs middle-xs middle-sm middle-md middle-lg">
                 <div class="col-xs-12">
                     <form action="">
-                        <input type="text" name="brugernavn" placeholder="Brugernavn...">
-                        <input type="password" name="password" placeholder="Password...">
-                        <button>Log in</button>
+                        <input class="log-in-box" type="text" name="brugernavn" placeholder="Brugernavn...">
+                        <input class="log-in-box" type="password" name="password" placeholder="Password...">
+                        <button id="log-in">Log in</button>
                     </form>
                 </div>
             </div>
-            <div class="row end-xs end-sm end-md end-lg center-xs middle-xs middle-sm middle-md middle-lg">
-                <div class="col-xs-12"> <a href="#" alt="Registrer dig her knap">Registrér dig her...</a> </div>
+            <div class="row end-xs end-sm end-md end-lg center-xs middle-xs middle-sm middle-md middle-lg ">
+                <div class="col-xs-12 margin-bot"> <a href="#" alt="Registrer dig her knap">Registrér dig her...</a> </div>
             </div>
         </div>
     </header>
@@ -106,21 +114,23 @@
     </main>
     <footer id="main-footer">
         <div class="footer-container">
-            <div class="row  center-xs  ">
-                <div class="col-xs-4 ">
+            <div class="row  middle-xs ">
+                <div class="col-xs-4 border-right text-center">
                     <p>Islændere i DK</p>
                     <p>Tågehornvej 32</p>
                     <p>9300 Ølstykke</p>
                     <p>islandere(at)live.com</p>
                     <p><i class="fa fa-phone" aria-hidden="true"></i> 12345678</p>
                 </div>
-                <div class="col-xs-4">
+                <div class="col-xs-4 border-right text-center">
                     <p>Mandag-fredag:</p>
                     <p>08:00-13:00</p>
                     <p>Lørdag:</p>
                     <p>09:00-11:00</p>
                 </div>
-                <div class="col-xs-4"> <img src="img/guy.jpg" alt="Thomas Karse img"> <span>Thomas Karse</span> <img src="img/guy2.jpg" alt="Bjarne Arm img"> <span>Bjarne Arm</span> <img src="img/guy3.jpg" alt="Lisbeth Grøn img"> <span>Lisbeth Grøn</span> </div>
+                <div class="col-xs-4 "> <img src="img/guy-edit.jpg" alt="Thomas Karse img"> <span>Thomas Karse</span>
+                    <br> <img src="img/guy2-edit.jpg" alt="Bjarne Arm img"> <span>Bjarne Arm</span>
+                    <br> <img src="img/guy3-edit.jpg" alt="Lisbeth Grøn img"> <span>Lisbeth Grøn</span> </div>
             </div>
         </div>
     </footer>
